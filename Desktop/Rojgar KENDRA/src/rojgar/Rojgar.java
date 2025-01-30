441043ca678286a9a4d367685af355169f1beef5
Rojan-2004 <rojanm1000@gmail.com>
2025-01-30 12:43:41 +0545

    Add jobs and proposal

---
+++ Desktop/Rojgar KENDRA/src/rojgar/Rojgar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rojgar;
+
+import rojgar.view.ApplicantProposal;
+
+/**
+ *
+ * @author rojan
+ */
+public class Rojgar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ApplicantProposal lf = new ApplicantProposal();
+        lf.setVisible(true);
+    }
+   
+    
+}
